5931b42a3e5e56dc432d7182e3129dfc1e20da4c
Sismarth <arthur.piqueras@e-artsup.net>
2023-06-20 15:35:02 +0200

    donjon
    2e partie finie

---
+++ Assets/_Core/Script/light custom/HackedLight.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Light))]
+public class HackedLight : MonoBehaviour {
+
+    public Vector4 hackColor;
+    public float multiplier = 1;
+
+    void Update ()
+    {
+        var light = GetComponent<Light>();
+        light.color = new Color(hackColor.x, hackColor.y, hackColor.z, hackColor.w) * multiplier;
+    }
+}
